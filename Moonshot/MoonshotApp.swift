eb5f1abbb35e433ef33398e25384f042372395b5
AsanoGenki <asanogenki0218@icloud.com>
2024-08-20 23:16:57 +0900

    Initial commit.

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Genki on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
